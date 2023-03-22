7e08d1f1ffa1f93d04fb86e296ace53abe4ca8c1
Chloe <52658941+wanqian-chen@users.noreply.github.com>
2023-03-22 01:31:44 +0000

    pet feeding

---
+++ week8/src/lib.rs
@@ -0,0 +1,106 @@
+// provide basic functions to play pet game
+
+// array to store pet species
+pub const PET_SPECIES: [&str; 3] = ["dog", "cat", "fish"];
+
+// struct to store pet information
+pub struct Pet {
+    pub name: String,
+    pub species: String,
+    pub food: u32,
+    pub bath: u32,
+    pub play: u32,
+}
+
+// initialize a pet
+pub fn init_pet(name: String, species: String) -> Pet {
+    let pet = Pet {
+        name,
+        species,
+        food: 0,
+        bath: 0,
+        play: 0,
+    };
+    pet
+}
+
+// feed a pet
+pub fn feed_pet(pet: &mut Pet) {
+    // if food is less than 90, add 10
+    if pet.food < 90 {
+        pet.food += 10;
+    }
+    // if food is more than 90, set food to 100
+    if pet.food > 90 {
+        pet.food = 100;
+    }
+    println!("You fed your {} {}, and its food level is now {}.", pet.species, pet.name, pet.food);
+}
+
+// give a pet a bath
+pub fn give_pet_a_bath(pet: &mut Pet) {
+    // if bath is less than 90, add 10
+    if pet.bath < 90 {
+        pet.bath += 10;
+    }
+    // if bath is more than 90, set bath to 100
+    if pet.bath > 90 {
+        pet.bath = 100;
+    }
+    println!("You gave your {} {} a bath, and its bath level is now {}.", pet.species, pet.name, pet.bath);
+}
+
+// play with a pet
+pub fn play_with_pet(pet: &mut Pet) {
+    // if play is less than 90, add 10
+    if pet.play < 90 {
+        pet.play += 10;
+    }
+    // if play is more than 90, set play to 100
+    if pet.play > 90 {
+        pet.play = 100;
+    }
+    println!("You played with your {} {}, and its play level is now {}.", pet.species, pet.name, pet.play);
+}
+
+// check the status of a pet
+pub fn check_pet_status(pet: &Pet) -> String {
+    // if food is less than 50, the pet is hungry
+    if pet.food < 50 {
+        format!("Your {} {} is hungry.", pet.species, pet.name)
+    }
+    // if bath is less than 50, the pet is dirty
+    if pet.bath < 50 {
+        format!("Your {} {} is dirty.", pet.species, pet.name)
+    }
+    // if play is less than 50, the pet is bored
+    if pet.play < 50 {
+        format!("Your {} {} is bored.", pet.species, pet.name)
+    }
+    // if food, bath, and play are all greater than 50, the pet is happy
+    if pet.food >= 50 && pet.bath >= 50 && pet.play >= 50 {
+        format!("Your {} {} is happy.", pet.species, pet.name)
+    }
+}
+
+// instruct the pet
+pub fn instruct_pet(pet: &mut Pet, instruction: String) {
+    // if any num is less than 5, notify the user
+    if pet.food < 5 {
+        println!("Your {} {} is too hungry to {}.", pet.species, pet.name, instruction);
+    }
+    else if pet.bath < 5 {
+        println!("Your {} {} is too dirty to {}.", pet.species, pet.name, instruction);
+    }
+    else if pet.play < 5 {
+        println!("Your {} {} is too bored to {}.", pet.species, pet.name, instruction);
+    }
+    else {
+        // do the instruction
+        println!("Your {} {} {}.", pet.species, pet.name, instruction);
+        // decrease food, bath, and play by 5
+        pet.food -= 5;
+        pet.bath -= 5;
+        pet.play -= 5;
+    }
+}
--- week8/src/main.rs
+++ week8/src/main.rs
@@ -1,3 +1,91 @@
-fn main() {
-    println!("Hello, world!");
+// a command-line tool to play pet-feeding
+
+use std::io;
+use clap::Parser;
+
+use pet_feeding::{init_pet, feed_pet, give_pet_a_bath, play_with_pet, check_pet_status};
+
+#[derive(Parser)]
+#[clap(
+    version = "1.0",
+    author = "Wanqian",
+    about = "Play pet-feeding"
+)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Option<Commands>,
 }
+
+#[derive(Parser)]
+enum Commands {
+    #[clap(version = "1.0", author = "Wanqian")]
+    PetFeeding {
+        #[clap(short, long)]
+        name: String,
+        #[clap(short, long)]
+        species: String,
+    },
+}
+
+fn main() {
+    let args = Cli::parse();
+    // welcome message
+    println!("=====================================");
+    println!("Welcome to the pet-feeding game!");
+    println!("=====================================");
+    
+    // initialize a pet
+    println!("Please enter your pet's name and species.");
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    let name = input.trim().to_string();
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    let species = input.trim().to_string();
+    let mut pet = init_pet(name, species);
+    println!("You have a pet named {} and it is a {}.", pet.name, pet.species);
+    println!("=====================================");
+    
+    loop {
+        println!("Please choose an action: 1. feed, 2. bath, 3. play, 4. check status, 5. instruct your pet to do something.");
+        println!("To quit the game, type \"quit\".");
+        // get the choice
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+        let choice = input.trim().to_string();
+        // play the game
+        if choice == "quit" {
+            println!("You quit the game.");
+            break;
+        }
+        if choice == "1" {
+            pet = feed_pet(pet);
+        } else if choice == "2" {
+            pet = give_pet_a_bath(pet);
+        } else if choice == "3" {
+            pet = play_with_pet(pet);
+        } else if choice == "4" {
+            check_pet_status(pet);
+        } else if choice == "5" {
+            println!("Please enter what you want your pet to do.");
+            let mut input = String::new();
+            std::io::stdin().read_line(&mut input).unwrap();
+            let action = input.trim().to_string();
+            pet = pet.do_something(action);
+        } else {
+            println!("Invalid choice.");
+        }
+
+        // ask if want to continue
+        println!("Do you want to continue? (y/n)");
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+        let choice_continue = input.trim().to_string();
+        if choice_continue == "n" {
+            println!("You quit the game.");
+            break;
+        }
+        println!("=====================================");
+    }
+}
+
